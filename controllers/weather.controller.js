f12140e3542d166aecf5c0ca16a9cd157b623f1d
njumbi <wambuinjumbi@gmail.com>
2021-02-21 22:09:34 +0300

    add axios to request data from weather api

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -1,9 +1,32 @@
-var unirest = require("unirest");
+var axios = require("axios");
+var momnet = require('moment');
 
 exports.getHomePage = (req, res, next) => {
     res.render('home');
 }
 
-exports.postWeather = async (req, res, next) => {
+exports.postWeather = (req, res, next) => {
+    // get params
+    var cityName = req.body.cityName;
+
+    // make request
+    var headers = {
+        "x-rapidapi-key": "90606781a8mshcc86af6d9febc62p1bfd7djsn4d1c8a43cd33",
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+        "useQueryString": true
+    }
+    axios.get(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${cityName}`, {
+            headers: headers
+        }).then(response => {
+
+            // format to days using moment
+            var day = moment().format('dddd');  
+
+            res.json(response)
+        })
+        .catch(error => {
+            console.log(error);
+            res.json(error)
+        });
 
 }
--- public/home.js
+++ public/home.js
@@ -1,19 +1,28 @@
-function initialize() {
-    var input = document.getElementById('locationField');
-
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        var place = autocomplete.getPlace();
-        //  alert(place.name + "  " + place.geometry.location.lat() + " " + place.geometry.location.lng());
-        $('#progress_bar').show()
-    });
-}
 $(document).ready(function () {
     // hide ui
     $('#weather_data').hide()
     $('#progress_bar').hide()
 
-    // init google places auto complete
-    google.maps.event.addDomListener(window, 'load', initialize);
+    // on button click
+    $('#searchButton').click(function () {
+        // check if city name is not empty
+        var cityName = $('#locationField').val();
+        if (cityName == '') {
+            alert('Enter city name');
+        } else {
+            // display progress bar
+            $('#progress_bar').show()
 
+            // create request data
+            var requestData = {
+                'cityName': cityName
+            }
+            // make request to controller
+            $.post("/weather", requestData, function (responseData, status) {
+                $('#progress_bar').hide()
+                // console.log(responseData);
+                console.log(status);
+            });
+        }
+    });
 });
